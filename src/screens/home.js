12beef48e30f0f4030ad0eba686e99275b6e633f
manaceia <manaceia@gmail.com>
2023-03-14 08:55:54 +0200

    SuperHeroes project commit

---
+++ src/screens/home.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Card from "../components/card";
+
+const Home = () => {
+  return (
+    <div id="home" className="min-h-screen min-h-screen">
+      <Card resource={`publishers`} limit={5} />
+      <Card resource={`superheroes`} limit={5} />
+    </div>
+  );
+};
+
+export default Home;
